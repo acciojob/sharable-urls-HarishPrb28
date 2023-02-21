fed982467c6d72bc4e2702bb7d6885c4f8457a07
Harish  Parab <50201375+HarishPrb28@users.noreply.github.com>
2023-02-21 18:01:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+function concatText(){
+	name = document.getElementById("name").value
+	year = document.getElementById("year").value
+	url= document.getElementById("url")
+	if(name && year){
+		url.innerHTML =`https://localhost:8080/?${name}&${year}`
+	}
+	else if(name && !year){
+		url.innerHTML =`https://localhost:8080/?${name}`
+	}
+	else if(!name && year){
+		url.innerHTML =`https://localhost:8080/?${year}`
+	}
+}
